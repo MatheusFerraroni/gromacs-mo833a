141ed694ad7ece7f1a998fed780b5c1c7c689c3c
Matheus <matheussanches531@gmail.com>
2020-04-20 22:14:02 -0300

    resultados do perf e valgrind com imagens e comparando com 4 e 1 thread

--- experiments/Final/p1.sh
+++ experiments/Final/p1.sh
@@ -1,10 +1,16 @@
 # rm perf.data
 # rm result1.txt
 # rm result2.txt
+# rm result3.txt
+# rm result4.txt
 # rm result_do_force1.txt
 # rm result_do_force2.txt
+# rm result_do_force3.txt
+# rm result_do_force4.txt
 # rm result_report_1.txt
 # rm result_report_2.txt
+# rm result_report_3.txt
+# rm result_report_4.txt
 # rm callgrind.out.*
 
 
@@ -23,5 +29,28 @@
 
 
 
+
+# rm perf.data
+# perf record ../../build/bin/gmx mdrun -v -nt 1 -deffnm em
+# perf script -f | cut -d: -f3 | sort | uniq -c | sort -n > result3.txt
+# perf script -f | cut -d: -f3 | sort | uniq -c | sort -n | grep "do_force "> result_do_force3.txt
+# perf report | grep gmx > result_report_3.txt
+# mv perf.data perf3.data
+
+# rm perf.data
+# perf record -g ../../build/bin/gmx mdrun -v -nt 1 -deffnm em
+# perf script -f | cut -d: -f3 | sort | uniq -c | sort -n > result4.txt
+# perf script -f | cut -d: -f3 | sort | uniq -c | sort -n | grep "do_force "> result_do_force4.txt
+# perf report | grep gmx > result_report_4.txt
+# mv perf.data perf4.data
+
+
+
+
 # valgrind --tool=callgrind ../../build/bin/gmx mdrun -v -deffnm em
+# kcachegrind
+
+
+
+# valgrind --tool=callgrind ../../build/bin/gmx mdrun -v -nt 1 -deffnm em
 # kcachegrind
