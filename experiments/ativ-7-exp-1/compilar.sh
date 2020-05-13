eec71fdefd8fc1f52ef50befc888c22780d9c830
Matheus <matheussanches531@gmail.com>
2020-05-13 13:35:32 -0300

    script execucao

--- experiments/ativ-7-exp-1/compilar.sh
+++ experiments/ativ-7-exp-1/compilar.sh
@@ -1,8 +1,10 @@
 
-mkdir build
-cd ../../build/
+# mkdir build
+# cd ../../build/
 
 
-# cmake .. -DGMX_BUILD_OWN_FFTW=ON -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DGMX_MPI=on
-cmake .. -DGMX_BUILD_OWN_FFTW=ON
-make 
+# # cmake .. -DGMX_BUILD_OWN_FFTW=ON -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DGMX_MPI=on
+# cmake .. -DGMX_BUILD_OWN_FFTW=ON
+# make 
+
+# ls
---
+++ experiments/ativ-7-exp-1/exec.sh
@@ -0,0 +1,4 @@
+cd conf
+
+gro='../../../build/bin/gmx'
+$gro mdrun -v -deffnm em 1> gmx.out 2> gmx.err
