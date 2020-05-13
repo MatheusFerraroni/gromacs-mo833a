630e6d83730e99b72fdf43309900696c9fd9cea7
Matheus <matheussanches531@gmail.com>
2020-05-13 17:09:04 -0300

    script setup

--- experiments/ativ-7-exp-1/compilar.sh
+++ experiments/ativ-7-exp-1/compilar.sh
@@ -1,6 +1,4 @@
 
-# mkdir build
-# cd ../../build/
 
 
 # # cmake .. -DGMX_BUILD_OWN_FFTW=ON -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DGMX_MPI=on
---
+++ script_setup.sh
@@ -0,0 +1,23 @@
+
+
+sudo apt-get update
+sudo apt-get install -y wget make gcc libgfortran3 \
+sysstat libibnetdisc-dev openmpi-bin libopenmpi-dev \
+libhdf5-openmpi-dev gfortran build-essential git
+
+
+wget https://github.com/Kitware/CMake/releases/download/v3.17.2/cmake-3.17.2-Linux-x86_64.sh
+chmod +x cmake-3.17.2-Linux-x86_64.sh
+bash cmake-3.17.2-Linux-x86_64.sh --skip-license
+sudo ln -s /home/ubuntu/cmake-3.17.2-Linux-x86_64/bin/* /usr/local/bin
+
+
+git clone --single-branch --branch ativi-7-exp-1 https://github.com/MatheusFerraroni/gromacs-mo833a.git
+cd gromacs-mo833a/
+
+mkdir build
+cd build
+
+
+cmake .. -DGMX_BUILD_OWN_FFTW=ON -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DGMX_MPI=on
+make
