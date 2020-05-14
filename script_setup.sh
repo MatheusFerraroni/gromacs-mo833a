3950bfb0dd15d2d7bf7e0766f2864dd9db19216e
Matheus <matheussanches531@gmail.com>
2020-05-14 10:41:23 -0300

    testando criacao da chave

--- script_setup.sh
+++ script_setup.sh
@@ -12,12 +12,15 @@ bash cmake-3.17.2-Linux-x86_64.sh --skip-license
 sudo ln -s /home/ubuntu/bin/* /usr/local/bin
 
 
-git clone --single-branch --branch ativi-7-exp-1 https://github.com/MatheusFerraroni/gromacs-mo833a.git
-cd gromacs-mo833a/
+# git clone --single-branch --branch ativi-7-exp-1 https://github.com/MatheusFerraroni/gromacs-mo833a.git
+# cd gromacs-mo833a/
 
-mkdir build
-cd build
+# mkdir build
+# cd build
+
+# cmake .. -DGMX_BUILD_OWN_FFTW=ON -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DGMX_MPI=on 1> cmake.out 2> cmake.outerr
+# make -i 1> make_res.out 2> make_res.outerr
 
 
-cmake .. -DGMX_BUILD_OWN_FFTW=ON -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DGMX_MPI=on 1> cmake.out 2> cmake.outerr
-make -i 1> make_res.out 2> make_res.outerr
+
+ssh-keygen -q -b 2048 -t rsa -N '' -C 'creating SSH' -f ~/.ssh/id_rsa creates='~/.ssh/id_rsa'
