edfa52e570bc65fda556cc899907ace1c7e371ad
Matheus <matheussanches531@gmail.com>
2020-05-14 11:32:47 -0300

    ajuste pro ssh

--- script_setup.sh
+++ script_setup.sh
@@ -1,15 +1,15 @@
 
 
 sudo apt-get update
-sudo apt-get install -y wget make gcc libgfortran3 \
-sysstat libibnetdisc-dev openmpi-bin libopenmpi-dev \
-libhdf5-openmpi-dev gfortran build-essential git
+# sudo apt-get install -y wget make gcc libgfortran3 \
+# sysstat libibnetdisc-dev openmpi-bin libopenmpi-dev \
+# libhdf5-openmpi-dev gfortran build-essential git
 
 
-wget https://github.com/Kitware/CMake/releases/download/v3.17.2/cmake-3.17.2-Linux-x86_64.sh
-chmod +x cmake-3.17.2-Linux-x86_64.sh
-bash cmake-3.17.2-Linux-x86_64.sh --skip-license
-sudo ln -s /home/ubuntu/bin/* /usr/local/bin
+# wget https://github.com/Kitware/CMake/releases/download/v3.17.2/cmake-3.17.2-Linux-x86_64.sh
+# chmod +x cmake-3.17.2-Linux-x86_64.sh
+# bash cmake-3.17.2-Linux-x86_64.sh --skip-license
+# sudo ln -s /home/ubuntu/bin/* /usr/local/bin
 
 
 # git clone --single-branch --branch ativi-7-exp-1 https://github.com/MatheusFerraroni/gromacs-mo833a.git
